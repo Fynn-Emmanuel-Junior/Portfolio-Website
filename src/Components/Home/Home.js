ba860de5bb794f59eb5b17f07d65f4acd08c77cc
Fynn Emmanuel Junior <fynn.emmanuel100@gmail.com>
2022-07-29 07:18:06 +0000

    Added the Portfolio Componenet

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,31 +2,61 @@ import Logo from "./Logo";
 import Navbar from "./Navbar";
 import About  from "../About/About";
 import Services from "../Services/Services";
-import {FaCheck,FaClipBoardList} from "react-icons";
+import Portfolio from '../Portfolio/Portfolio';
+import { FaCheck,FaClipboardList,FaUserFriends,FaAward } from "react-icons/fa";
 export default function Home() {
     return (
         <div style={{width:'100%',height:'700px', position: 'relative'}}>
+
             <div className='Header' style={{width: '100%', height: '100%', position: 'relative'}}>
                 <div className="overlay">
                 </div>
+
                 <div className="header-container" style={{width: '80%', display: 'flex', justifyContent: 'space-between', margin: 'auto',position: 'relative', top: '20px'}}>
                 <Logo />
                 <Navbar /> 
                 </div>
-                <div className="header-text" style={{position: 'relative', fontSize: '70px', textAlign: 'center'}}>
+
+                <div className="header-text">
                   <p>I am Morgan Freeman</p>
-                  <p className="text" style={{fontSize: '50px', marginTop: '10px', borderRight: '2px solid #fff'}}>Freelancer</p>
+                  <p className="text">Freelancer</p>
                 </div>
             </div>
+
           <About />
           <Services />
-          <div className="main-1" style={{width: '100%', height: '350px'}}>
+
+          <div className="main-1">
             <div className="overlay-1">
             </div>
             <div className="section-icons">
-
+              <div style={{display: 'flex',justifyContent: 'space-evenly', width: '100%'}}>
+                <div>
+                  <FaCheck  className="icon" />
+                  <h3 className="heading">450</h3>
+                  <p>WORKS COMPLETED</p>
+                </div> 
+                <div>
+                  <FaClipboardList  className="icon"/>
+                  <h3 className="heading">25</h3>
+                  <p>YEARS OF EXPERINECE</p>
+                </div>
+                <div>
+                  <FaUserFriends className="icon"/>
+                  <h3 className="heading">450</h3>
+                  <p>TOTAL CLIENTS</p>
+                </div> 
+                <div>
+                  <FaAward className="icon"/>
+                  <h3 className="heading">450</h3>
+                  <p>WORKS COMPLETED</p>
+                </div>
+              </div>
             </div>
           </div>
+
+          <Portfolio />
+
         </div>
     )
 }
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,13 +2,13 @@ export default function Services() {
     return (
         <div className="MainContainer" style={{width: '100%',height: '500px',position: 'relative',marginTop: '120px'}}>
             <div style={{textAlign: 'center'}}>
-                <h2 style={{fontSize: '50px'}}>Services</h2>
+                <h2 style={{fontSize: '50px'}}>Portfolio</h2>
                 <p></p>
             </div>
             <div style={{width: "100%",position: 'relative'}}>
                 <div className="sections-1" >
                     <div className="side" >
-
+                    
                     </div>
                     <div className="side" >
 
